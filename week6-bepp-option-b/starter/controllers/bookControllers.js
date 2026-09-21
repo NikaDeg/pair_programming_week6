45c9d1911f77eaa52acce20c236659176f917ef3
Aakash-Basnet <aakashb@metropolia.fi>
2026-09-21 14:21:22 +0300

    feat:add deletebook controller

--- week6-bepp-option-b/starter/controllers/bookControllers.js
+++ week6-bepp-option-b/starter/controllers/bookControllers.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 
 // GET /books
 const getAllBooks = async (req, res) => {
-  try{
-    const books = await Book.find({}).sort({createdAt: -1});
+  try {
+    const books = await Book.find({}).sort({ createdAt: -1 });
     res.status(200).json(books);
-  }catch(error){ 
-    res.status(500).json({message: "Failed to retrieve books", error: error.message})
-   }
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve books', error: error.message });
+  }
 };
 
 // POST /books
@@ -33,7 +33,21 @@ const updateBook = async (req, res) => {
 
 // DELETE /books/:bookId
 const deleteBook = async (req, res) => {
-  res.send('deleteBook');
+  const { bookId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(bookId)) {
+    return res.status(400).json({ message: 'invalid book id' });
+  }
+
+  try {
+    const deleteBook = await Book.findOneAndDelete({ _id: bookId });
+    if (deleteBook) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Book not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete book' });
+  }
 };
 
 module.exports = {
