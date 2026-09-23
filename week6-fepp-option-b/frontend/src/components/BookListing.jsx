c40fd28926f75ed819aca19ef766836c5b0f0505
Aakash-Basnet <aakashb@metropolia.fi>
2026-09-23 14:26:12 +0300

    feat(book-page): add route and fetch single book by id

--- week6-fepp-option-b/frontend/src/components/BookListing.jsx
+++ week6-fepp-option-b/frontend/src/components/BookListing.jsx
@@ -1,15 +1,18 @@
-const BookListing = ({book}) => {
+import { Link } from 'react-router-dom';
+
+const BookListing = ({ book }) => {
   return (
     <div className="book-preview">
-      <h2>Book Title: {book.title}</h2>
-      <p>Author: {book.author}</p>
-      <p>ISBN: {book.isbn}</p>
-      <p>Publisher: {book.publisher}</p>
-      <p>Genre: {book.genre}</p>
-      <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
+      <Link to={`/books/${book.id}`}>
+        <h2>Book Title: {book.title}</h2>
+        <p>Author: {book.author}</p>
+        <p>ISBN: {book.isbn}</p>
+        <p>Publisher: {book.publisher}</p>
+        <p>Genre: {book.genre}</p>
+        <p>Available: {book.availability.isAvailable ? 'Yes' : 'No'}</p>
+      </Link>
     </div>
   );
 };
 
 export default BookListing;
-
